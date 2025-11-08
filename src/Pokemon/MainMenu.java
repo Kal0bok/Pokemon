cb3a39a3b7f27879ef87138b043baf08b5826374
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-08 15:28:37 +0200

    Izveidots logs un tas nosaukums

--- src/Pokemon/MainMenu.java
+++ src/Pokemon/MainMenu.java
@@ -1,7 +1,22 @@
 package Pokemon;
 
-public class MainMenu {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-	//start
+public class MainMenu {
+	JFrame frame = new JFrame("Game");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 300);
+    frame.setResizable(false);
+    frame.setLayout(new BorderLayout());
+    frame.getContentPane().setBackground(new Color(20, 30, 60));
+
+
+    JLabel title = new JLabel("POKEMON GAME", SwingConstants.CENTER);
+    title.setFont(new Font("Arial", Font.BOLD, 36));
+    title.setForeground(Color.YELLOW);
+    title.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
+    frame.add(title, BorderLayout.NORTH);
 	
 }
