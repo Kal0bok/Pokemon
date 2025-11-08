789d87550aa9a225196132d249c9dfc8d6dfd7b4
Bogdan <bogdan1ptlvt@gmail.com>
2025-11-08 15:29:32 +0200

    Izveidoju pogus start un exit

--- src/Pokemon/MainMenu.java
+++ src/Pokemon/MainMenu.java
@@ -1,7 +1,12 @@
 package Pokemon;
 
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 public class MainMenu {
@@ -18,5 +23,20 @@ public class MainMenu {
     title.setForeground(Color.YELLOW);
     title.setBorder(BorderFactory.createEmptyBorder(20, 0, 20, 0));
     frame.add(title, BorderLayout.NORTH);
+    
+    JPanel buttonPanel = new JPanel(new GridLayout(2, 1, 15, 15));
+    buttonPanel.setBorder(BorderFactory.createEmptyBorder(20, 100, 40, 100));
+    buttonPanel.setBackground(new Color(20, 30, 60));
+
+    JButton startBtn = createButton("START", Color.GREEN);
+    JButton exitBtn = createButton("EXIT", Color.RED);
+
+    startBtn.addActionListener(e -> JOptionPane.showMessageDialog(frame, "Game starts!"));
+    exitBtn.addActionListener(e -> System.exit(0));
+
+    buttonPanel.add(startBtn);
+    buttonPanel.add(exitBtn);
+
+    frame.add(buttonPanel, BorderLayout.CENTER);
 	
 }
